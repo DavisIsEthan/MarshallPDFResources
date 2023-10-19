0f03f577acab026d9d1ad7322ec7870bd0bee99b
DavisIsEthan <davisethanp@gmail.com>
2023-10-19 13:58:36 -0400

    Update IntakeSplitRename.py
    Now handles case that entire folder is one document.

--- IntakeSplitRename.py
+++ IntakeSplitRename.py
@@ -11,33 +11,63 @@ SCRIPT FOR EASIER SPLITTING AND NAMING OF MARHSALL PDFs
 
 ### imports
 import os
+import sys
 from pypdf import PdfWriter, PdfReader
 
 #### new j for box 056 = 20
 j = 1
-box = "57"
-num = "001"
-split = [1, 2, 3, 4, 5, 6, 7]
+box = "59"
+num = "002"
+split = [1]
 
 
 fpath = f"C:/Users/Davis/Desktop/MarshallPDFProject/Box{box}/Box0{box}/"
 fname = f"MarshallPentagonSelected_B0{box}_F{num}.pdf"
 reader = PdfReader(fpath+fname)
-print(len(reader.pages))
-print(reader.pages)
+
+
 
 outpath = f"C:/Users/Davis/Desktop/MarshallPDFProject/Box{box}_Output/Box0{box}/"
 outpathExist = os.path.exists(outpath)
 if not outpathExist:
     os.makedirs(outpath)
-
-for i in range(0, (len(split))):
+    
+    
+### Handles the case that the folder contains one document    
+if len(split) == 1:
     merger = PdfWriter()
+    
+    ### Name output file nicely 
+    hold = str(j).rjust(3, "0")
+    outname = f"MarshallPentagonSelected_B0{box}_F{num}_{hold}.pdf"
+    outfile = outpath+outname
+    print(outname)
+    
+    ### append pages to output
+    merger.append(reader)
+    output = open(outfile, 'wb')
+    merger.write(output)
+    merger.close()
+    output.close()
+    
+    ### iterate j
+    j = j + 1
+    
+    print(f"Success. New J = {j}")
+    
+    ### quit program here so we don't double up on outputs
+    sys.exit()
+    
+### Handles the case that there are multiple documents in folder
+for i in range(0, (len(split))):
+    merger = PdfWriter()      
+    
+    ### Handles last entry in split
     if i == (len(split)-1):
         if len(split) == len(reader.pages):
-            span = split[i] - split[i-1]
+            span = split[i] - split[i-1] ### last entry = 1 page out
         else:
-            span = len(reader.pages) - (split[i]-1)
+            span = len(reader.pages) - (split[i]-1) ### last entry = many page out
     else: 
         span = split[i+1] - split[i] ### length of each sub document
 
